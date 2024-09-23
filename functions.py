60c54c6a3c583c5796f537ea32ae3676b28d702b
nitya <nitya.taurus@gmail.com>
2024-09-23 10:50:32 +0200

    Implemented thr Edit button

--- functions.py
+++ functions.py
@@ -1,11 +1,18 @@
-def get_todos(filepath='todos.txt'):
-    """Read a text file and return the list of to do items"""
-    with open(filepath,'r')as file_local:
+FILEPATH = "todos.txt"
+
+
+def get_todos(filepath=FILEPATH):
+    """ READ A TEXT FILE AND RETURN THE LIST OF
+        TO-DO ITEMS
+
+    """
+    with open(filepath, 'r') as file_local:
         todos_local = file_local.readlines()
     return todos_local
 
-def write_todos(todos_arg,filepath='todos.txt'):
 
-    with open(filepath,'w')as file:
-        file.writelines(todo_arg)
+def write_todos(todos_arg, filepath=FILEPATH):
+    """Write the to-do items in the next file."""
 
+    with open(filepath, 'w') as file:
+        file.writelines(todos_arg)
